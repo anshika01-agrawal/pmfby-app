7ec8ab27c591aa579ca6329b7107ecccae3de9cc
anshika01-agrawal <23btc106@ietdavv.edu.in>
2025-11-24 09:45:09 +0000

    Fix import paths in officer_dashboard_screen and remove duplicate imports

--- lib/src/features/multi_image/batch_upload_progress_screen.dart
+++ lib/src/features/multi_image/batch_upload_progress_screen.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '../../services/image_upload_service.dart';
 import 'package:intl/intl.dart';
-import '../../services/image_upload_service.dart';
 
 class BatchUploadProgressScreen extends StatefulWidget {
   const BatchUploadProgressScreen({super.key});
